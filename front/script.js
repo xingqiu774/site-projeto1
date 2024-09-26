c61f418ffc82448ef139abf38f02c29378aad1e8
Lewis (Jio) <62856711+xingqiu774@users.noreply.github.com>
2024-09-26 20:43:58 -0300

    Update script.js

--- front/script.js
+++ front/script.js
@@ -125,6 +125,11 @@ function check_for_tag(projetos, arquivos, target, pid) {
         
             has_or_not = 1;
 
+            
+            
+
+
+            /*
             const myArticle = document.createElement("h2");
             myArticle.style.textAlign = "center";
             myArticle.classList.add("infobox");
@@ -138,8 +143,54 @@ function check_for_tag(projetos, arquivos, target, pid) {
             myDesc.textContent = projeto.descricao;
             myDesc.style.textAlign = "center";
             myArticle.appendChild(myDesc);
+            
+            
 
-            section.appendChild(myArticle);
+            */
+
+            let div = document.createElement("div");
+            div.classList.add("bloco");
+            let thumbnail = document.createElement("img");
+            //thumbnail.src = "banjo.jpg";
+            thumbnail.style.height = "30%"
+            thumbnail.style.width = "50%";
+
+            let title = document.createElement("span");
+            title.textContent = projeto.nome;
+            title.style.marginTop = "10px";
+            title.style.marginBottom = "10px";
+            title.classList.add("poppins-extrabold");
+
+            let desc = document.createElement("span");
+            desc.textContent = projeto.descricao;
+
+            let dateSymbol = document.createElement("span");
+            dateSymbol.classList.add("material-symbols-outlined");
+            dateSymbol.textContent = "calendar_month";
+
+            let dateDate = document.createElement("span");
+            dateDate.textContent = projeto.data_envio;
+
+            let dateText = document.createElement("span");
+            dateText.style.marginTop = "auto";
+            dateText.style.marginRight = "auto";
+
+            let author = document.createElement("span");
+            author.textContent = "Autor Desconhecido";
+            author.style.marginRight = "auto";
+
+            dateText.appendChild(dateSymbol);
+            dateText.appendChild(dateDate);
+
+            if (thumbnail != null)  {
+                div.appendChild(thumbnail);
+            }
+            div.appendChild(title);
+            div.appendChild(desc);
+            div.appendChild(dateText);
+            div.appendChild(author);
+
+            section.appendChild(div);
         
     }
 
